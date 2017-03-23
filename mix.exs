654fc2632edefaf0db8b89b01087bd83c334f58e
ryan <ryan@rtv-mac-mini.local>
2017-03-22 22:29:01 -0700

    Release v0.1.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Lexicon.Mixfile do
 
   def project do
     [app: :lexicon,
-     version: "0.1.2-dev",
+     version: "0.1.2",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
